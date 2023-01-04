b3ce5a54185be4ff3301726f18bbb3c992bd9940
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-01-03 21:56:23 -0500

    act arch

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
-import { useState } from "react";
-import mapa from "../public/img/mapa_mudo_01.jpg";
-import logo from "../public/img/rpw.png";
+import logo from "../src/assets/logo_v2.png.png";
+import mapa from "../src/assets/mapa_mudo_01.jpg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <h1>
         <img src={logo} alt="mapa" width="60" height="40" /> Real Pixel World
       </h1>
-      <div>
-        <canvas id="mapa" width="1024" height="768">
-          <img src={mapa} alt="mapa" width="1024" height="768"/>
-        </canvas>
-        <p id="clear">clear</p>
-        <script src="main.js"></script>
-      </div>
+        <img src={mapa} alt="mapa" width="1024" height="768"/>
+
     </div>
   );
 }
