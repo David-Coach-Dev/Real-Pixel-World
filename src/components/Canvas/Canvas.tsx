91a46e4d5eaf21d68a6b46e0554254b9f97e0482
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-01-04 17:51:45 -0500

    arch act cuadricula

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -7,17 +7,64 @@ export interface CanvasInterface {}
 const Canvas: React.FC<CanvasInterface> = () => {
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
-
   const [isDrawing, setIsDrawing] = useState(false);
   const [isColor, setColor] = useState("");
+  const [toggleGuide, setToggleGuide] = useState(true);
   const [x, setX] = useState(1280);
 	const [y, setY] = useState(780);
-	const [a, setA] = useState(0);
-	
+  const [a, setA] = useState(0);
+ 
 	useEffect(() => {
-		const canvas = canvasRef.current;
-		const context = canvas.getContext("2d");
-		if (a === 0) {
+    const guide = document.getElementById("guide");
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    guide.style.width = `${canvas.width}px`;
+    guide.style.height = `${canvas.height}px`;
+    const CELL_SIDE_COUNT = 100;
+    const cellPixelLength = canvas.width / CELL_SIDE_COUNT;
+    const colorHistory = {};
+    guide.style.gridTemplateColumns = `repeat(${CELL_SIDE_COUNT}, 1fr)`;
+    guide.style.gridTemplateRows = `repeat(${CELL_SIDE_COUNT}, 1fr)`;
+    guide.style.display = toggleGuide ? null : "none";
+    //
+     canvas.addEventListener("mousedown", handleCanvasMousedown);
+     function handleCanvasMousedown(e) {
+       // Ensure user is using their primary mouse button
+       if (e.button !== 0) {
+        return;
+       }
+
+       const canvasBoundingRect = canvas.getBoundingClientRect();
+       const x = e.clientX - canvasBoundingRect.left;
+       const y = e.clientY - canvasBoundingRect.top;
+       const cellX = Math.floor(x / cellPixelLength);
+       const cellY = Math.floor(y / cellPixelLength);
+       const currentColor = colorHistory[`${cellX}_${cellY}`];
+
+       if (e.ctrlKey) {
+         if (currentColor) {
+           setColor(currentColor);
+         }
+       } else {
+         fillCell(cellX, cellY);
+       }
+     }
+    function fillCell(cellX, cellY) {
+      const startX = cellX * cellPixelLength;
+      const startY = cellY * cellPixelLength;
+
+      context.fillStyle = isColor;
+      context.fillRect(startX, startY, cellPixelLength, cellPixelLength);
+      colorHistory[`${cellX}_${cellY}`] = isColor;
+    }
+
+    //sfgdf
+    if (a === 0) {
+      context.fillStyle = "#ffffff";
+      context.fillRect(0, 0, canvas.width, canvas.height);
+      [...Array(CELL_SIDE_COUNT ** 2)].forEach(() =>
+        guide.insertAdjacentHTML("beforeend", "<div></div>")
+      );
 			canvas.width = x;
 			canvas.height = y;
 			context.lineCap = "round";
@@ -75,6 +122,7 @@ const Canvas: React.FC<CanvasInterface> = () => {
 
   return (
     <CanvasStyle>
+      <CanvasGuide id="guide"></CanvasGuide>
       <div>
         <canvas
           className="canvas-container"
@@ -111,6 +159,14 @@ export const CanvasStyle = styled.div`
   height: 780px;
   border: 2px solid #323232;
   background-color: #fff;
+  cursor: pointer;
+`;
+export const CanvasGuide = styled.div`
+  display: grid;
+  pointer-events: none;
+  position: absolute;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+
 `;
 
 export default Canvas;
