532cdfee79a93af1f6ac3d373cd2af167122b367
Dc Dev Rep <69812733+DcDevRep@users.noreply.github.com>
2023-01-05 20:25:37 -0500

    act arch

--- src/components/Canvas/Canvas copy.tsx
+++ src/components/Canvas/Canvas copy.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
-import logo from 'https://i.imgur.com/dslrfVI.png';
 
 export interface CanvasInterface {}
 
@@ -9,46 +8,41 @@ const Canvas: React.FC<CanvasInterface> = () => {
   const contextRef = useRef(null);
   const [isDrawing, setIsDrawing] = useState(false);
   const [isColor, setColor] = useState("#834745");
+  const [toggleGuide, setToggleGuide] = useState(true);
   const [isX, setIsX] = useState(0);
   const [isY, setIsY] = useState(0);
   const [isPixelX, setIsPixelX] = useState(0);
   const [isPixelY, setIsPixelY] = useState(0);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const logo = 'https://i.imgur.com/dslrfVI.png';
-  const fondo = "./src/assets/mapa_mudo_01.png";
-  let mapaHistory = {};
-  const colorHistory = {};
+  const [a, setA] = useState(0);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
-    //Cargando Canvas
-    if (!isLoaded) {
+    const CELL_SIDE_COUNT = 500;
+    const cellPixelLength = canvas.width / CELL_SIDE_COUNT;
+    const colorHistory = {};
+    //
+    if (a === 0) {
+      context.fillStyle = "#E47979";
       canvas.width = (window.innerWidth);
       canvas.height = (window.innerHeight);
       contextRef.current = context;
-      setIsLoaded(true);
-      let img = new Image();
-      img.src = fondo;
-      console.log(img.src);
-      const onLoad = () => {
-        context.drawImage(img, 0, 0, canvas.width, canvas.height);
-        console.log("ok");
-      };
-      onLoad();
+      setA(1);
+      const img = new Image();
+      img.src = "../../assets/";
+      context.drawImage(img, 0, 0);
     }
-    const CELL_SIDE_COUNT = 500;
-    const cellPixelLengthX = canvas.width / CELL_SIDE_COUNT;
-    const cellPixelLengthY = canvas.height / CELL_SIDE_COUNT;
     //
-    
+    canvas.addEventListener("mousedown", handleCanvasMousedown);
+    window.addEventListener("mousemove", handleWindowMousemove);
+
     function handleWindowMousemove(e) {
       const canvasBoundingRect = canvas.getBoundingClientRect();
       setIsX(
-        Math.floor((e.clientX - canvasBoundingRect.left) / cellPixelLengthX)
+        Math.floor((e.clientX - canvasBoundingRect.left) / cellPixelLength)
       );
       setIsY(
-        Math.floor((e.clientY - canvasBoundingRect.top) / cellPixelLengthY)
+        Math.floor((e.clientY - canvasBoundingRect.top) / cellPixelLength)
       );
     }
     function handleCanvasMousedown(e) {
@@ -56,8 +50,10 @@ const Canvas: React.FC<CanvasInterface> = () => {
         return;
       }
       const canvasBoundingRect = canvas.getBoundingClientRect();
-      const cellX = Math.floor((e.clientX - canvasBoundingRect.left) / cellPixelLengthX);
-      const cellY = Math.floor((e.clientY - canvasBoundingRect.top) / cellPixelLengthY);
+      const x1 = e.clientX - canvasBoundingRect.left;
+      const y1 = e.clientY - canvasBoundingRect.top;
+      const cellX = Math.floor(x1 / cellPixelLength);
+      const cellY = Math.floor(y1 / cellPixelLength);
       setIsPixelX(cellX);
       setIsPixelY(cellY);
       const currentColor = colorHistory[`${cellX}_${cellY}`];
@@ -70,19 +66,32 @@ const Canvas: React.FC<CanvasInterface> = () => {
       }
     }
     function fillCell(cellX, cellY) {
-      const startX = cellX * cellPixelLengthX;
-      const startY = cellY * cellPixelLengthY;
+      const startX = cellX * cellPixelLength;
+      const startY = cellY * cellPixelLength;
       context.fillStyle = isColor;
-      context.fillRect(startX, startY, cellPixelLengthX, cellPixelLengthY);
+      context.fillRect(startX, startY, cellPixelLength, cellPixelLength);
       colorHistory[`${cellX}_${cellY}`] = isColor;
     }
-    canvas.addEventListener("mousedown", handleCanvasMousedown);
-    window.addEventListener("mousemove", handleWindowMousemove);
     context.strokeStyle = isColor;
-  }, [isColor]);
+  }, [isColor, toggleGuide, a]);
 
   const handleColorChange = (e) => {
     setColor(e.target.value);
+  };
+
+  const handleToggleGuide = () => {
+    setToggleGuide(!toggleGuide);
+  };
+
+  const draw = ({ nativeEvent }) => {
+    if (!isDrawing) {
+      return;
+    }
+  };
+
+  const stopDrawing = () => {
+    contextRef.current.closePath();
+    setIsDrawing(false);
   };
 
   const setToDraw = () => {
@@ -96,15 +105,6 @@ const Canvas: React.FC<CanvasInterface> = () => {
   const setToClear = () => {
     contextRef.current.clearRect(0, 0, window.innerWidth, window.innerHeight);
   };
-  const setToSave = () => {
-    let image = canvasRef.current.toDataURL("image/png");
-    console.log(image);
-    let link = document.getElementById("download_image_link");
-    link.setAttribute("download", "canvas.png");
-    link.setAttribute("target", "_blank");
-    link.click();
-    link.setAttribute("href", image);
-  };
 
   const saveImageToLocal = (event) => {
     let link = event.currentTarget;
@@ -116,16 +116,19 @@ const Canvas: React.FC<CanvasInterface> = () => {
   return (
     <>
       <CanvasStyle>
+        <CanvasGuide id="guide"></CanvasGuide>
         <canvas
           className="canvas-container"
           ref={canvasRef}
+          onMouseMove={draw}
+          onMouseUp={stopDrawing}
+          onMouseLeave={stopDrawing}
         ></canvas>
       </CanvasStyle>
       <CanvasControle>
         <button onClick={setToDraw}>Draw</button>
         <button onClick={setToErase}>Erase</button>
         <button onClick={setToClear}>Clear</button>
-        <button onClick={setToSave}>Save</button>
         <label id="coor_x">
           x = {isX} - y = {isY}{" "}
         </label>
@@ -139,6 +142,7 @@ const Canvas: React.FC<CanvasInterface> = () => {
             handleColorChange(e);
           }}
         />
+        <button onClick={handleToggleGuide}>Toggle Guide</button>
         <a
           id="download_image_link"
           href="download_link"
@@ -153,6 +157,12 @@ const Canvas: React.FC<CanvasInterface> = () => {
 
 export const CanvasStyle = styled.div`
   cursor: pointer;
+  border: 1px solid #c01d1d;
+`;
+export const CanvasGuide = styled.div`
+  display: grid;
+  pointer-events: none;
+  position: absolute;
   border: 1px solid #c01d1d;
 `;
 export const CanvasControle = styled.div`
